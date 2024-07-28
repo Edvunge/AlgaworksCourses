57b444cc1a2a7ce25470b44fa402084a63753b7d
edvaldo <costaedy18@gmail.com>
2024-07-28 23:56:25 +0100

    new_course

---
+++ src/especialistaJava/section01_plataforma_e_ambienteDesenvolvimento/HelloWorld.java
@@ -0,0 +1,7 @@
+package especialistaJava.section01_plataforma_e_ambienteDesenvolvimento;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Ola, Mundo!");
+    }
+}
---
+++ src/especialistaJava/section02_fundamentosJava/OlaMergulhador.java
@@ -0,0 +1,8 @@
+package especialistaJava.section02_fundamentosJava;
+
+public class OlaMergulhador {
+	public static void main(String[] args) {
+
+		System.out.println("Ola ,Mergulhador!");
+	}
+}
