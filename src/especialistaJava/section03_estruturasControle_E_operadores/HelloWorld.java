051a9eba97f8fd5c24e9c9b3b2147e19d41c7a5b
Edvaldo vunge <costaedy18@gmail.com>
2024-10-13 10:03:21 +0100

    first_class

---
+++ src/especialistaJava/section03_estruturasControle_E_operadores/HelloWorld.java
@@ -0,0 +1,7 @@
+package especialistaJava.section03_estruturasControle_E_operadores;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("..........\n  Hello \n  World! \n..........");
+    }
+}
