500a7798cb8bad4d9bb29165040e41813173ef6b
edvaldovunge <costaedy18@gmail.com>
2024-12-21 00:54:02 +0000

    update

--- src/especialistaJava/section08_trabalhandoComArrays/arrays/OrdenandoUmArrayDeFormaNatural.java
+++ src/especialistaJava/section08_trabalhandoComArrays/arrays/OrdenandoUmArrayDeFormaNatural.java
@@ -8,6 +8,8 @@ public class OrdenandoUmArrayDeFormaNatural {
 
         Arrays.sort(notas);
 
-        System.out.println(notas);
+        for (int i = 0; i < notas.length; i++) {
+            System.out.println(notas[i]);
+        }
     }
 }
